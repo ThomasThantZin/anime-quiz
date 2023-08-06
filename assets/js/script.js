2dfe55393954ceda8e320e61dbf0ddfe95774e7f
ThomasThantZin <>
2023-08-06 12:22:52 +0000

    declare all variables in javascript and put require quiz images in images file

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//declare all variables
+let start = document.getElementById("start");
+let quiz = document.getElementById("quiz");
+let quizQuestion = document.getElementById("quizQuestion");
+let quizImg = document.getElementById("quizImg");
+let optionA = document.getElementById("choiceA");
+let optionB = document.getElementById("choiceB");
+let optionC = document.getElementById("choiceC");
+let optionD = document.getElementById("choiceD");
+let scoreBlock = document.getElementById("scoreBlock");
+let scoreMessage = document.getElementById("scoreMessage");
+let quizAgain = document.getElementById("quizAgain");
+let choices = document.getElementById("choices");
+let choiceResponse = document.getElementById("choiceResponse");
+let score = 0;
