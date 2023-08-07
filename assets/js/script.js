0d85b170fe37b2d8c36b68f62e37cec8bf9234af
ThomasThantZin <>
2023-08-07 11:29:16 +0000

    add all questions and answers with array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,101 @@
 //declare all variables
-let start = document.getElementById("start");
-let quiz = document.getElementById("quiz");
-let quizQuestion = document.getElementById("quizQuestion");
-let quizImg = document.getElementById("quizImg");
-let optionA = document.getElementById("choiceA");
-let optionB = document.getElementById("choiceB");
-let optionC = document.getElementById("choiceC");
-let optionD = document.getElementById("choiceD");
-let scoreBlock = document.getElementById("scoreBlock");
-let scoreMessage = document.getElementById("scoreMessage");
-let quizAgain = document.getElementById("quizAgain");
-let choices = document.getElementById("choices");
-let choiceResponse = document.getElementById("choiceResponse");
-let score = 0;
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var quizQuestion = document.getElementById("quizQuestion");
+var quizImg = document.getElementById("quizImg");
+var optionA = document.getElementById("choiceA");
+var optionB = document.getElementById("choiceB");
+var optionC = document.getElementById("choiceC");
+var optionD = document.getElementById("choiceD");
+var scoreBlock = document.getElementById("scoreBlock");
+var scoreMessage = document.getElementById("scoreMessage");
+var quizAgain = document.getElementById("quizAgain");
+var choices = document.getElementById("choices");
+var choiceResponse = document.getElementById("choiceResponse");
+var score = 0;
+
+//questions function so our getQuestion function later can get the right value from array
+
+let questions = [{
+    question: "Which anime is she from?",
+    imgSrc: "assets/images/azusa_nakano.png",
+    choiceA: "Naruto",
+    choiceB: "K-ON!",
+    choiceC: "Bocchi The Rock",
+    choiceD: "Love Live!",
+    correctAnswer: "B"
+}, {
+    question: "which anime is she from?",
+    imgSrc: "assets/images/kaori_miyazono.png",
+    choiceA: "Violet Evergarden",
+    choiceB: "I want to eat your pancreas",
+    choiceC: "Your Lie in April",
+    choiceD: "5 Centimeters per Second",
+    correctAnswer: "C"
+}, {
+    question: "Which 'Golden Time' anime charater is this?",
+    imgSrc: "assets/images/kouko_kaga.png",
+    choiceA: "Kouko Kaga",
+    choiceB: "Chinami Oka",
+    choiceC: "Banri Tada",
+    choiceD: "Nana Hayashida",
+    correctAnswer: "A"
+}, {
+    question: "Which anime series is she from?",
+    imgSrc: "assets/images/megumi_tadokoro.png",
+    choiceA: "Food Wars!: Shokugeki no Soma",
+    choiceB: "Isekai Restaurant",
+    choiceC: "Ben-To",
+    choiceD: "Yakitate!! Japan",
+    correctAnswer: "A"
+}, {
+    question: "Which anime series is she from?",
+    imgSrc: "assets/images/mikasa_ackerman.png",
+    choiceA: "Ajin: Demi-Human",
+    choiceB: "Heavenly Delusion",
+    choiceC: "Fullmetal Alchemist",
+    choiceD: "Attack On Titan",
+    correctAnswer: "D"
+}, {
+    question: "Which anime series is she from?",
+    imgSrc: "assets/images/miyuki_shiba.png",
+    choiceA: "The Irregular at Magic High School",
+    choiceB: "The Asterisk War",
+    choiceC: "High School DxD",
+    choiceD: "High School of the Dead",
+    correctAnswer: "A"
+}, {
+    question: "Which anime series is she from?",
+    imgSrc: "assets/images/sahoto_ashisu.png",
+    choiceA: "The Pet Girl Of Sakurasou",
+    choiceB: "Eromanga Sensei",
+    choiceC: "Keep Your Hands Off Eizouken",
+    choiceD: "Mangaka-san to Assistant",
+    correctAnswer: "D"
+}, {
+    question: "Which 'Haikyu!' anime charater is this?",
+    imgSrc: "assets/images/tooru_oikawa.png",
+    choiceA: "Shoyo Hinata",
+    choiceB: "Kenma Kozume",
+    choiceC: "Tooru Oikawa",
+    choiceD: "Wakatoshi Ushijima",
+    correctAnswer: "C"
+}, {
+    question: "Which anime is she from?",
+    imgSrc: "assets/images/uka-no-mitama-no-kami.png",
+    choiceA: "Kamisama Kiss",
+    choiceB: "Hotarubi no Mori e",
+    choiceC: "Inari, Konkon, Koi Iroha",
+    choiceD: "Noragami",
+    correctAnswer: "C"
+}, {
+    question: "Which anime is she from?",
+    imgSrc: "assets/images/yukino_yukinoshita.png",
+    choiceA: "My Teen Romantic Comedy SNAFU",
+    choiceB: "KoKoro Connect",
+    choiceC: "Seitokai Yakuindomo",
+    choiceD: "Classroom of the Elite",
+    correctAnswer: "A"
+},];
+
+
